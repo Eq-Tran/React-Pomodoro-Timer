c1cfe183384aaab498f865795b831743e0f76111
Eq-Tran <Eqtrann@gmail.com>
2026-08-21 13:25:33 -0400

    Initial text field with submit functioning correctly

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ToDoList = () => {
+    const [task, setTask] = useState("");
+
+    function handleChange(e) {
+        setTask(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(task);
+    }
 
 	return (
 		<section>
             <h2>To-Do List</h2>
-            <label htmlFor='name-input'>Add a task: </label>
-            <text>Text here</text>
-            <input type="checkbox"/>
+            <form onSubmit={handleSubmit}>
+                <label>Tasks:
+                    <input 
+                    type="text"
+                    value={task}
+                    onChange={handleChange} />
+                </label>
+                <input type="submit"/>
+            </form>
 		</section>
 	);
 };
