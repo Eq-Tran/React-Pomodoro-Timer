eb6aa99a4a48d1081aec80959a64cec530979fd0
Eq-Tran <Eqtrann@gmail.com>
2026-08-21 12:48:03 -0400

    Started to do list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Timer from './components/Timer.jsx';
+import ToDoList from './components/ToDoList.jsx';
 
 // main application function
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <div>
         <Timer />
+        <ToDoList />
       </div>
     </>
   )
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ToDoList = () => {
+
+	return (
+		<section>
+            <h2>To-Do List</h2>
+            <label htmlFor='name-input'>Add a task: </label>
+            <text>Text here</text>
+            <input type="checkbox"/>
+		</section>
+	);
+};
+
+export default ToDoList;
