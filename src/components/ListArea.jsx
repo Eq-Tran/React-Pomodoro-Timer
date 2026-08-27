d33b4488cb77b5ba3c475397347720f7e30ac681
Eq-Tran <Eqtrann@gmail.com>
2026-08-26 22:52:26 -0400

    added checkbox and delete button to list area items, changed button sizings

--- src/components/ListArea.jsx
+++ src/components/ListArea.jsx
@@ -5,7 +5,17 @@ const ListArea = ({ taskData }) => {
 		<div className="list-area">
 			<ul>
 				{taskData.map((task, index) => (
-					<li key={`${task}-${index}`}>{task}</li>
+					<li key={`${task}-${index}`}>
+						{/* Added a label and checkbox for marking each task. */}
+						<label className="list-item-label">
+							<input type="checkbox" />
+							<span>{task}</span>
+						</label>
+						{/* Added an unbound delete button for future React functionality. */}
+						<button type="button" aria-label={`Delete ${task}`}>
+							Delete
+						</button>
+					</li>
 				))}
 			</ul>
 		</div>
